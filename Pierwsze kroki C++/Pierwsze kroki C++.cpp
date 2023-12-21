6d1ce4208e6b530ecbf115420dbf269e943089f1
Bauti139 <115225330+Bauti139@users.noreply.github.com>
2023-12-21 20:40:34 +0100

    Stworzenie pliku i podstawy
    Stworzyłem plik oraz dodałem podstawowe komendy jak przypisywanie wartości, odbieranie wartości od użytkownika.

---
+++ Pierwsze kroki C++/Pierwsze kroki C++.cpp
@@ -0,0 +1,37 @@
+﻿// Pierwsze kroki C++.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+/*int add(int a, int b)
+{
+    a = a + b;
+    return a;
+}*/
+int main()
+{
+    /*int a;
+    int b;
+    double c = 2.5;
+    cout << "Please enter number for a" << endl;
+    cin >> a;
+    cout << "Please enter number for b" << endl;
+    cin >> b;
+    double i[] = { 190, 2, 3, a, b, c };
+    cout << add(a, b) << endl;
+    cout << i[5];*/
+    int a = 2;
+    int b = 3;
+    cout << a / b << endl;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
